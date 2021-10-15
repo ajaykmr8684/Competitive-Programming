7c080009b62e99604be534b1f070807d7085da73
Kumar <ajay.kumar@nokia.com>
2021-10-15 19:41:25 +0530

    Selection Sort

---
+++ 8. Selection Sort.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+void selectSort(int arr[], int n) {
+    for (int i = 0; i < n; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; ++j) {
+
+            if (arr[minIndex] > arr[j]) {
+                minIndex = j;
+            }
+
+        }
+
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+
+
+
+int main() {
+
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    selectSort(arr, n);
+
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+
+}
+
+
+
